1594aa2f4f6aa483871ba0fe9d0e905a4d100972
smalldolphin <sueprsmalldolphin@gmail.com>
2012-02-14 16:47:24 +0800

    first commit

---
+++ MyFirstAndroid/src/com/smalldolphin/myandroid/MyFirstAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.smalldolphin.myandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyFirstAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
